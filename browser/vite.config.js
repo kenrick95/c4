8dcb5af35a041e0611484bbf8d8f7a1d7f9050d0
Kenrick <kenrick95@gmail.com>
2023-04-20 22:43:29 +0800

    fix: make build work for browser

--- browser/vite.config.js
+++ browser/vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({})
+export default defineConfig({
+  resolve: {
+    conditions: ['__source', 'import', 'module', 'browser', 'default'],
+  },
+})
